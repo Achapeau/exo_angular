15aeb579fc941174fadceb10fa14f780eb5ec318
Adrien <ad.chap.13.17@gmail.com>
2024-04-15 16:03:52 +0200

    add taskService

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -4,6 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TaskService {
+  Tasks: Object[] = [
+    {
+      title: 'Task 1',
+      description: 'Description 1'
+    },
+    {
+      title: 'Task 2',
+      description: 'Description 2'
+    },
+    {
+      title: 'Task 3',
+      description: 'Description 3'
+    }
+  ]
 
   constructor() { }
+
+  getTasks() {
+    return this.Tasks
+  }
+  
+
+  setTask(title: string, description: string) {
+    this.Tasks = [...this.Tasks, { title, description }]
+  }
 }
